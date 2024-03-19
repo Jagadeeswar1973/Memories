7316a6054178f748c7c2c86a49bac7121df86d7f
Jagadeeswar1973 <150112098+Jagadeeswar1973@users.noreply.github.com>
2024-03-19 16:45:08 +0530

    Add files via upload

---
+++ client/src/components/Posts/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+//import usestyles from "./style";
+
+export default makeStyles((theme) => ({
+  mainContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  smMargin: {
+    margin: theme.spacing(1),
+  },
+  actionDiv: {
+    textAlign: 'center',
+  },
+}));
